a0a17365e8d1fd384c81cd04d80a56c479ca2858
rasmu234 <rasmussonntag@gmail.com>
2021-05-24 23:01:35 +0200

    not sure xd

--- models/score.js
+++ models/score.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const scoreSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: true
+        required: true,
     },
     score: {
         type: Number,
