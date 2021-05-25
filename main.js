238a5c5d4a2b800817059ffa676bf650b9a8dce0
rasmu234 <rasmussonntag@gmail.com>
2021-05-25 13:43:07 +0200

    remove .env

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
-
 const mongoose = require("mongoose")
-const express = require("express");
+const express = require("express")
 const app = express()
 const cors = require("cors")
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect(process.env.DATABASE_URL || 5000, {useNewUrlParser: true})
+mongoose.connect("mongodb+srv://snakeMaster:snake@snakecluster.cbj1k.mongodb.net/snakeMaster?retryWrites=true&w=majority", {useNewUrlParser: true})
 const db = mongoose.connection
 db.on("error", (error) => console.error(error))
 db.once("open", () => console.log("connected to database"))
 
 
 const usersRouter = require("./routes/scores")
-app.use("/scores",usersRouter)
+app.use("/scores", usersRouter)
 
-app.listen(process.env.PORT,() => console.log("server started"))
+app.listen(process.env.PORT || 5000, () => console.log("server started"))
