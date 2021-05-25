670e69c393dec508b29f8e92280d41eaf88ccc98
rasmu234 <rasmussonntag@gmail.com>
2021-05-25 13:39:42 +0200

    remove .env

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-require("dotenv").config()
+
 const mongoose = require("mongoose")
 const express = require("express");
 const app = express()
@@ -6,7 +6,7 @@ const cors = require("cors")
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
+mongoose.connect(process.env.DATABASE_URL || 5000, {useNewUrlParser: true})
 const db = mongoose.connection
 db.on("error", (error) => console.error(error))
 db.once("open", () => console.log("connected to database"))
